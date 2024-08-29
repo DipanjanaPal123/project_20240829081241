9b854e884df150d602a5e4333f7c464fca4a9a08
DipanjanaPal123 <146447816+DipanjanaPal123@users.noreply.github.com>
2024-08-29 22:38:09 +0530

    Add files via upload

---
+++ Page2.jsx
@@ -0,0 +1,91 @@
+import './Page2.css'
+
+export default function Page2() {
+  return (
+    <div className="page-2">
+      <div className="vector-15">
+      </div>
+      <div className="clip-path-group-16">
+        <div className="clip-path-group-17">
+          <div className="rectangle-7">
+          </div>
+          <div className="clip-path-group-18">
+            <div className="rectangle-8">
+            </div>
+          </div>
+        </div>
+        <img className="container-3" src="assets/vectors/Container_x2.svg" />
+        <div className="clip-path-group-6">
+          <div className="rectangle">
+          </div>
+        </div>
+        <div className="clip-path-group-7">
+          <div className="rectangle-1">
+          </div>
+        </div>
+        <div className="clip-path-group-8">
+          <div className="rectangle-2">
+          </div>
+        </div>
+        <div className="clip-path-group-9">
+          <div className="rectangle-3">
+          </div>
+        </div>
+        <div className="clip-path-group-10">
+          <div className="rectangle-4">
+          </div>
+        </div>
+        <img className="clip-path-group-13" src="assets/vectors/ClipPathGroup5_x2.svg" />
+        <div className="clip-path-group-14">
+          <div className="rectangle-5">
+          </div>
+        </div>
+        <span className="rohit-dubey">
+        Rohit Dubey
+        </span>
+        <span className="ui-ux-designer">
+        UI/ UX Designer
+        </span>
+        <span className="singh">
+        Singh
+        </span>
+        <span className="mohit">
+        Mohit
+        </span>
+        <span className="backend-developer">
+        Backend Developer
+        </span>
+        <span className="marshal-fein">
+        Marshal Fein
+        </span>
+        <span className="frontend-developer">
+        Frontend Developer
+        </span>
+        <span className="rahul-roy">
+        Rahul Roy
+        </span>
+        <span className="social-media-manager">
+        Social Media Manager
+        </span>
+        <span className="abhishek-sen">
+        Abhishek Sen
+        </span>
+        <span className="content-writer">
+        Content Writer
+        </span>
+        <span className="pranay-kaur">
+        Pranay Kaur
+        </span>
+        <span className="graphic-designer">
+        Graphic Designer
+        </span>
+        <span className="our-top-frelancers">
+        Our top frelancers
+        </span>
+        <span className="why-choose-us">
+        Why choose us ?
+        </span>
+      </div>
+    </div>
+  )
+}
