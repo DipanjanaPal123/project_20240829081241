ffc5e19e9b58515af118aad612874a76ee397473
DipanjanaPal123 <146447816+DipanjanaPal123@users.noreply.github.com>
2024-08-29 22:37:17 +0530

    Add files via upload

---
+++ Page1.jsx
@@ -0,0 +1,94 @@
+import './Page1.css'
+
+export default function Page1() {
+  return (
+    <div className="page-1">
+      <div className="vector-60">
+      </div>
+      <div className="clip-path-group-8">
+        <div className="clip-path-group-15">
+          <img className="vector-11" src="assets/vectors/Vector2_x2.svg" />
+        </div>
+        <div className="clip-path-group-19">
+          <img className="vector-18" src="assets/vectors/Vector5_x2.svg" />
+        </div>
+        <img className="container-1" src="assets/vectors/Container1_x2.svg" />
+        <div className="clip-path-group-5">
+          <img className="vector-3" src="assets/vectors/Vector3_x2.svg" />
+        </div>
+        <div className="clip-path-group-6">
+          <img className="vector-4" src="assets/vectors/Vector6_x2.svg" />
+        </div>
+        <div className="clip-path-group-7">
+          <img className="clip-path-group-11" src="assets/vectors/ClipPathGroup4_x2.svg" />
+          <img className="clip-path-group-14" src="assets/vectors/ClipPathGroup2_x2.svg" />
+          <img className="clip-path-group-18" src="assets/vectors/ClipPathGroup3_x2.svg" />
+          <img className="clip-path-group-22" src="assets/vectors/ClipPathGroup6_x2.svg" />
+          <img className="clip-path-group-25" src="assets/vectors/ClipPathGroup7_x2.svg" />
+          <div className="group-2">
+            <img className="vector-5" src="assets/vectors/Vector1_x2.svg" />
+          </div>
+          <div className="clip-path-group-26">
+            <img className="group-8" src="assets/vectors/Group_x2.svg" />
+          </div>
+          <img className="container-10" src="assets/vectors/Container2_x2.svg" />
+          <div className="clip-path-group-67">
+            <div className="rectangle-6">
+            </div>
+          </div>
+          <span className="popular-services">
+          Popular Services
+          </span>
+          <span className="singh">
+          Singh
+          </span>
+          <span className="rani">
+          Rani
+          </span>
+          <span className="backend-developer">
+          Backend Developer
+          </span>
+          <span className="arindam-guha">
+          Arindam Guha
+          </span>
+          <span className="frontend-developer">
+          Frontend Developer
+          </span>
+          <span className="pratik-roy">
+          Pratik Roy
+          </span>
+          <span className="social-media-manager">
+          Social Media Manager
+          </span>
+          <span className="ram-mandal">
+          Ram Mandal
+          </span>
+          <span className="content-writer">
+          Content Writer
+          </span>
+          <span className="adrish-sen">
+          Adrish Sen
+          </span>
+          <span className="graphic-designer">
+          Graphic Designer
+          </span>
+          <span className="fauget">
+          Fauget
+          </span>
+          <span className="search-for-freelancers">
+          Search for freelancers
+          </span>
+          <span className="sign-in">
+          SIGN IN
+          </span>
+          <span className="explore">
+          Explore
+          </span>
+          <span className="our-services">
+          Our Services
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
