7f2e29ff973de913d68cce5c136b490e95f2d98f
DipanjanaPal123 <146447816+DipanjanaPal123@users.noreply.github.com>
2024-08-29 22:39:05 +0530

    Add files via upload

---
+++ Page3.jsx
@@ -0,0 +1,63 @@
+import './Page3.css'
+
+export default function Page3() {
+  return (
+    <div className="page-3">
+      <div className="vector">
+      </div>
+      <div className="clip-path-group-1">
+        <div className="clip-path-group-2">
+          <div className="rectangle">
+          </div>
+        </div>
+        <div className="clip-path-group-3">
+          <div className="clip-path-group-4">
+            <img className="vector-3" src="assets/vectors/Vector4_x2.svg" />
+          </div>
+          <div className="clip-path-group-5">
+            <img className="vector-4" src="assets/vectors/Vector_x2.svg" />
+          </div>
+        </div>
+        <img className="clip-path-group-8" src="assets/vectors/ClipPathGroup_x2.svg" />
+        <div className="clip-path-group-9">
+          <div className="vector-8">
+          </div>
+        </div>
+        <div className="clip-path-group-10">
+          <div className="vector-9">
+          </div>
+        </div>
+        <div className="clip-path-group-11">
+          <div className="vector-10">
+          </div>
+        </div>
+        <div className="clip-path-group-12">
+          <div className="rectangle-1">
+          </div>
+        </div>
+        <div className="clip-path-group-13">
+          <div className="rectangle-2">
+          </div>
+        </div>
+        <img className="clip-path-group-16" src="assets/vectors/ClipPathGroup1_x2.svg" />
+        <div className="clip-path-group-17">
+          <div className="vector-17">
+          </div>
+          <img className="container" src="assets/vectors/Container3_x2.svg" />
+        </div>
+        <span className="get-started">
+        Get Started
+        </span>
+        <p className="freelancing-made-easy">
+        <span className="freelancing-made-easy-sub-0"></span><span></span>
+        </p>
+        <span className="fauget-international-ltd-2024">
+        @ Fauget International Ltd. 2024
+        </span>
+        <span className="fauget">
+        Fauget
+        </span>
+      </div>
+    </div>
+  )
+}
